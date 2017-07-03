e97d47cb5e820bc583422bf4101f9b01b39b3dd2
高子健 <gaozijian0411@outlook.com>
2017-07-03 18:02:33 +0800

    mongodb测试成功

--- serverModules/UserManager.js
+++ serverModules/UserManager.js
@@ -3,22 +3,18 @@ const Route=new Router()
 const assert=require("assert")
 const mongoose=require("mongoose")
 mongoose.Promise=Promise;
-Route.all("/",async (ctx,next)=>{
-    ctx.body="hello world";
 
-    mongoose.connect("mongodb://192.168.1.170/test",{},(err)=>{
+mongoose.connect("mongodb://localhost/test",{},(err)=>{
         console.log(err)
     })
 let TestModel=mongoose.model("TestModel",{
     name:String,
     password:String
 })
-let me=new TestModel({name:"gaozijian",password:"testpass"})
-me.name="高子建";
-me.set("password","testtest")
-me.save((err)=>{console.log(err)});
-mongoose.disconnect();
+
+Route.all("/",async (ctx,next)=>{
+    ctx.body="hello world";
+    
 })
-
-
+Route.all("/add")
 module.exports=Route;
