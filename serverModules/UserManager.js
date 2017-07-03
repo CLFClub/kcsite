6d4f54278ba6089b4b9ceda8ecbe79868203c485
高子健 <gaozijian0411@outlook.com>
2017-07-03 16:57:52 +0800

    mongodb测试未成功

--- serverModules/UserManager.js
+++ serverModules/UserManager.js
@@ -1,7 +1,24 @@
 const Router=require("koa-router")
 const Route=new Router()
-Route.all("/test",async (ctx,next)=>{
+const assert=require("assert")
+const mongoose=require("mongoose")
+mongoose.Promise=Promise;
+Route.all("/",async (ctx,next)=>{
     ctx.body="hello world";
+
+    mongoose.connect("mongodb://192.168.1.170/test",{},(err)=>{
+        console.log(err)
+    })
+let TestModel=mongoose.model("TestModel",{
+    name:String,
+    password:String
 })
+let me=new TestModel({name:"gaozijian",password:"testpass"})
+me.name="高子建";
+me.set("password","testtest")
+me.save((err)=>{console.log(err)});
+mongoose.disconnect();
+})
+
 
 module.exports=Route;
